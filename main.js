dfb67311b7e4c265771542d351f288e0aa00532b
Amr <abazov1477@gmail.com>
2019-02-10 20:33:30 +0300

    3

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+function throughOnce(fn) {
+  var i = 0;
+  return function(text) {
+    i == 0 && fn(text);
+    i = i == 0 ? 1 : 0;
+  }
+}
 
+var logThroughOnce = throughOnce(function(text) {
+  console.log(text);
+});
+
+logThroughOnce("Hello World!"); // "Hello World!"
+
+logThroughOnce("Hello World!"); //
+
+logThroughOnce("Hello World!"); // "Hello World!"
+
+logThroughOnce("Hello World!"); //
